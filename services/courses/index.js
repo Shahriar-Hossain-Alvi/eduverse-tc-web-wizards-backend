ebdbb32dec96a2d240b2d75137542a28c91638fb
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 13:46:29 +0600

    created a api for both get and get a course

--- services/courses/index.js
+++ services/courses/index.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 // create all course
 router.post("/", require("./views/creat-course"));
+// get all course
+router.get("/", require("./views/get-course"));
+// Get a course by id
+router.get("/:id", require("./views/get-a-course"));
 
 module.exports = router;
