f59f49f1f9ecf9127937ba43292ddca87a82ed77
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 13:43:26 +0600

    get router add

--- services/courses/views/get-a-course.js
+++ services/courses/views/get-a-course.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const ErrorResponse = require("../../../utils/middleware/error/error.response");
+const Course = require("../schema/course.schema");
 
 
 // get all courses
@@ -7,16 +9,29 @@ module.exports = async (req, res, next) => {
         params: { id },
     } = req;
 
-   
+    if (!id) {
+        return next(new ErrorResponse("id is required", 400))
+    }
+
+    // check if the id is a valid mongodb id
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(new ErrorResponse("Invalid course ID", 400));
+    }
 
     try {
-        
+        // check if the course exists or not
+        const result = await Course.findById(id).select("-__v");
+
+        // if course is not found
+        if (!result) {
+            return next(new ErrorResponse(`There are no course found with this ID: ${id}`, 404))
+        }
 
         // send response
         res.status(200).json({
             success: true,
             message: "Course fetched successfully",
-            
+            data: result
         });
 
     } catch (error) {
