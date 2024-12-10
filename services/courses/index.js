075f92b5c1d7c9dfcac2d2b7556a1f203a8dfde5
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 14:19:34 +0600

    created  a api for update a course

--- services/courses/index.js
+++ services/courses/index.js
@@ -7,5 +7,7 @@ router.post("/", require("./views/creat-course"));
 router.get("/", require("./views/get-course"));
 // Get a course by id
 router.get("/:id", require("./views/get-a-course"));
+// delete a course by id
+router.delete("/:id", require("./views/delete-course"))
 
 module.exports = router;
