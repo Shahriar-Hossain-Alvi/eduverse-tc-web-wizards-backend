10103ff1f5ad0684e046184471785fda733c7f6c
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2024-12-10 19:26:13 +0600

    added verifytoken for classes api

--- services/classes/index.js
+++ services/classes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../../utils/middleware/token-verification/auth.middleware")
 
 // create all classes
-router.post("/", require("./views/create-classes"));
+router.post("/",verifyToken, require("./views/create-classes"));
 
 // get all classes
-router.get("/", require("./views/get-classes"));
+router.get("/", verifyToken, require("./views/get-classes"));
 
 // get all classes
-router.get("/:id", require("./views/get-a-classes"));
+router.get("/:id",verifyToken, require("./views/get-a-classes"));
 
 module.exports = router;
