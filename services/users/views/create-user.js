6a9237fac8fe0d7cd7fd7471115cc30dbc824903
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2024-12-10 12:54:50 +0600

    fixed create user api function

--- services/users/views/create-user.js
+++ services/users/views/create-user.js
@@ -1,13 +1,19 @@
 const ErrorResponse = require("../../../utils/middleware/error/error.response");
 const User = require("../schema/user.schema");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { ACCESS_TOKEN_SECRET } = require("../../../configuration");
 
+
+// Create a new User 
 module.exports = async (req, res, next) => {
-	const { email, password, user_role = "student" } = req.body;
+	const { email, password, user_role, first_name, last_name } = req.body;
 
+	
 	try {
-		if (!email || !password || !user_role) {
+		if (!email || !password || !user_role || !first_name || !last_name) {
 			return next(
-				new ErrorResponse("Email, Password & Role are required!", 400)
+				new ErrorResponse("Email, Password, First name, Last name & Role are required!", 400)
 			);
 		}
 
@@ -17,14 +23,34 @@ module.exports = async (req, res, next) => {
 			return next(new ErrorResponse("User exist with the email!", 400));
 		}
 
+		// Generate a unique username
+		let user_name = `${first_name.toLowerCase()}_${last_name.toLowerCase()}`;
+		let count = 0;
+		while (await User.findOne({ user_name })) {
+			count++;
+			user_name = `${first_name.toLowerCase()}_${last_name.toLowerCase()}${count}`;
+		}
+
+		//Hash the password
+		const salt = await bcrypt.genSaltSync(10);
+		const hashedPass = await bcrypt.hashSync(password.toString(), salt);
+
 		// create a new user
-		const newUser = new User({ email, password_hashed: password, user_role });
+		const newUser = new User({ email, password_hashed: hashedPass, user_role, first_name, last_name, user_name });
 		// save the new user in the DB
 		const result = await newUser.save();
+
+
+		// Generate JWT token
+		const token = jwt.sign(
+			{ id: result._id, email: result.email }, ACCESS_TOKEN_SECRET, { expiresIn: '12h' }
+		);
+
 
 		res.status(201).json({
 			success: true,
 			message: `Account with ${result.email} email created successfully`,
+			token
 		});
 	} catch (error) {
 		// Send Error Response
