b627912597dfdc6ca73b6071a0904d22119a02d2
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 16:15:04 +0600

    created  a api for courseStudentEnrollment

--- services/courseStudentEnrollment/index.js
+++ services/courseStudentEnrollment/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/", require("./Views/create-courseStudentEnrollment"))
+
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+//show users list
 router.use("/users", require("./users"));
+
+//show courses list
 router.use("/courses", require("./courses"));
+
+//show courseMaterials list
 router.use("/courseMaterials", require("./courseMaterials"));
+
+//show CourseFacultyAssignments list
 router.use("/CourseFacultyAssignments", require("./CourseFacultyAssignments"));
+
+//show CourseFacultyAssignments list
+router.use("/courseStudentEnrollment", require("./courseStudentEnrollment"));
 
 module.exports = router;
