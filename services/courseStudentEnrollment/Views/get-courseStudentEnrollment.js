a8be066856333c5db14325dcf83dde964a6c6a73
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 16:40:11 +0600

    created apis for  get all & get a courseStudentEnrollment

---
+++ services/courseStudentEnrollment/Views/get-courseStudentEnrollment.js
@@ -0,0 +1,28 @@
+const ErrorResponse = require('../../../utils/middleware/error/error.response');
+const CourseStudentEnrollment = require('../schema/courseStudentEnrollment.schema');
+
+
+module.exports = async (req, res, next) => {
+    try {
+        // Fetch all course student enrollments with populated user and course details
+        const enrollments = await CourseStudentEnrollment.find()
+            .populate("users_id", "first_name last_name email") // Populate the user's first name, last name and email
+            .populate("course_id", "title description"); // Populate the course title and description
+
+        // If no enrollments found
+        if (enrollments.length === 0) {
+            return next(new ErrorResponse("No student enrollments found.", 404));
+        }
+
+        // Send success response
+        res.status(200).json({
+            success: true,
+            message: "Student enrollments fetched successfully.",
+            data: enrollments,
+        });
+
+    } catch (error) {
+        // send error response
+        next(error)
+    }
+}
--- services/courseStudentEnrollment/index.js
+++ services/courseStudentEnrollment/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", require("./Views/create-courseStudentEnrollment"))
+// create all courseStudentEnrollment
+router.post("/", require("./Views/create-courseStudentEnrollment"));
+
+// get all courseStudentEnrollment
+ router.get("/", require("./Views/get-courseStudentEnrollment"));
+
+// get a courseStudentEnrollment
+ router.get("/:id", require("./Views/get-a-courseStudentEnrollment"));
 
 module.exports = router;
