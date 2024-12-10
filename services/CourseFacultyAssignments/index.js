38c9cd59c44eb4ee9ca4686f0894430cd576cc41
Eusouf <riyajuddeneusouf@gmail.com>
2024-12-10 14:57:34 +0600

    created apis for get & get a courseFacultyAssignment

--- services/CourseFacultyAssignments/index.js
+++ services/CourseFacultyAssignments/index.js
@@ -4,5 +4,10 @@ const router = express.Router();
 // create all courseFacultyAssignment
 router.post("/", require("./Views/create-courseFacultyAssignment"));
 
+// get all courseFacultyAssignment
+router.get("/", require("./Views/get-courseFacultyAssignment"));
+
+// get all courseFacultyAssignment
+router.get("/:id", require("./Views/get-a-courseFacultyAssignment"));
 
 module.exports = router;
