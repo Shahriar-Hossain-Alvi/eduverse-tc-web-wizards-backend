7d891dd324952cc7de52add1895370ee48c71760
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-04-03 18:05:02 +0600

    created student dashboard overview api

---
+++ services/quickOverview/controllers/studentQuickOverview.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const Course = require("../../courses/schema/course.schema");
+const CourseStudentEnrollment = require("../../courseStudentEnrollment/schema/courseStudentEnrollment.schema")
+const Class = require("../../classes/schema/classes.schema")
+
+
+module.exports = async (req, res, next) => {
+
+    const { id } = req?.params; // student id
+
+    try {
+        // get total enrolled courses
+        const total_Enrolled_courses = await CourseStudentEnrollment.countDocuments({
+            users_id: { $in: [new mongoose.Types.ObjectId(id)] }
+        });
+
+
+
+        // get recently enrolled course
+        const get_Latest_Enrolled_Course = await CourseStudentEnrollment.find({
+            users_id: {
+                $in: [new mongoose.Types.ObjectId(id)]
+            }
+        }).sort({ createdAt: -1 })  // Sort by newest first
+            .limit(1).select("course_id");
+
+        const get_Latest_Enrolled_Course_Data = await Course.findById(get_Latest_Enrolled_Course[0].course_id).select("title start_date -_id")
+
+        const latest_Enrolled_Courses = {
+            latest_Enrolled_Course_Id: get_Latest_Enrolled_Course[0]._id,
+
+            latest_Enrolled_Course_title:
+                get_Latest_Enrolled_Course_Data.title,
+
+            latest_Enrolled_start_date:
+                get_Latest_Enrolled_Course_Data.start_date,
+        }
+
+
+        // gt upcoming class schedules
+        const now = new Date();
+
+        // find enrolled courses
+        const enrolledCourses = await CourseStudentEnrollment.find({
+            users_id: new mongoose.Types.ObjectId(id)
+        }).select("course_id");
+
+        const courseIds = enrolledCourses.map(enrollment => enrollment.course_id)
+
+        // get upcoming classes 
+        const upcoming_classes = await Class.find({
+            course_id: { $in: courseIds },
+            scheduled_time: { $gte: now }
+        }).sort({scheduled_time: 1}).limit(2).select("_id scheduled_time title")
+
+
+        const quickOverview = {
+            total_Enrolled_courses,
+            latest_Enrolled_Courses,
+            upcoming_classes
+        }
+
+
+        res.status(201).json({
+            data: quickOverview,
+            success: true,
+            message: "Overview fetched successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
--- services/quickOverview/index.js
+++ services/quickOverview/index.js
@@ -4,13 +4,16 @@ const verifyToken = require("../../utils/middleware/token-verification/auth.midd
 const verifyRole = require("../../utils/middleware/role-verification/verifyRole.middleware");
 
 
-router.use(verifyToken)
+router.use(verifyToken);
 
 // admin dashboard overview
 router.get("/admin", verifyRole("admin"), require("./controllers/adminQuickOverview"));
 
 // faculty quick overview
 router.get("/faculty/:id", verifyRole("faculty"), require("./controllers/facultyQuickOverview"));
+
+// student overview
+router.get("/student/:id", verifyRole("student"), require("./controllers/studentQuickOverview"));
 
 
 
